0c78dfdff92a19b883c9107cc2b8d942c5c96a8d
Shubham Pardeshi <93419446+shubhamp21898@users.noreply.github.com>
2022-11-04 15:05:23 +0530

    Add files via upload

---
+++ FabbonicSeries.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fibonacci_Series
+{
+    class FabbonicSeries
+    {
+        public void Fab(int Num)
+        {
+            int FIrstnum = 0, SecondNum = 1, NextNum;
+           
+
+            NextNum = FIrstnum + SecondNum;
+            Console.Write(FIrstnum + " " + SecondNum+" ");
+            for(int i=2;i<Num;i++)
+            {
+                NextNum = FIrstnum + SecondNum;
+                Console.Write(NextNum + " ");
+                FIrstnum = SecondNum;
+                SecondNum = NextNum;
+                
+            }
+            Console.ReadKey();
+        }
+       
+    }
+}
