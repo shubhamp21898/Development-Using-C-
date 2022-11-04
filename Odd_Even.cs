71438316ead959ee5d64b500486a67df8239362c
Shubham Pardeshi <93419446+shubhamp21898@users.noreply.github.com>
2022-11-04 15:06:00 +0530

    Add files via upload

---
+++ Odd_Even.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fibonacci_Series
+{
+    class Odd_Even
+    {
+       public Boolean Check_Even(int iVal)
+        {
+            if((iVal%2)==0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public void Even()
+        {
+            int No;
+            Console.WriteLine("Enter a No :");
+            No = Int32.Parse(Console.ReadLine());
+            Odd_Even obj = new Odd_Even();
+            bool EvenOdd_Bool = obj.Check_Even(No);
+                if (EvenOdd_Bool == true)
+            {
+                Console.WriteLine("Number is Even");
+            }
+            else
+            {
+                Console.WriteLine("Number is Odd");
+            }
+        }
+    }
+}
